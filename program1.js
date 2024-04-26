5d4c12b5ab080dfcbe10c21cef060974203b78d2
Arushi Sthapak <99334415+Arushi-Sthapak@users.noreply.github.com>
2024-04-26 13:21:12 +0530

    Update program1.js

--- program1.js
+++ program1.js
@@ -1,9 +1,39 @@
-function smallestMissingPositiveInteger(nums) {
+function smallestMissingPositiveInteger(A)
+    {
+        let n = A.length;
+  
+        let present = new Array(n+1);
+        
+        
+        for(let i=0;i<n+1;i++)
+        {
+            present[i]=false;
+        }
+        
+        for (let i = 0; i < n; i++)
+        {
+            if (A[i] > 0 && A[i] <= n)
+            {
+                present[A[i]] = true;
+            }
+        }
 
 
-    // Implement the function smallest_missing_positive_integer
-  
-  }
-  
-  module.exports = smallestMissingPositiveInteger;
+        for (let i = 1; i <= n; i++)
+        {
+            if (!present[i])
+            {
+                return i;
+            }
+        }
+        return n + 1;
+    }
+
+
+    module.exports = smallestMissingPositiveInteger;
+
+
+
+
+
 
