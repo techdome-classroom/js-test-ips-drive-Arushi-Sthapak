03b8d21c4b44d02386a62ea8e3e3d378e1c3303c
Arushi Sthapak <99334415+Arushi-Sthapak@users.noreply.github.com>
2024-04-26 13:22:30 +0530

    Update program2.js

--- program2.js
+++ program2.js
@@ -1,7 +1,18 @@
-function longestSubstring(s) {
+function lengthOfLongestSubstring(s) {
+    const n = s.length;
+    let longestLength = 0;
+    let start = 0;
+    const charIndexMap = {};
 
+    for (let end = 0; end < n; end++) {
+        const currentChar = s[end];
+        if (charIndexMap[currentChar] !== undefined && charIndexMap[currentChar] >= start) {
+            start = charIndexMap[currentChar] + 1;
+        }
+        charIndexMap[currentChar] = end;
+        longestLength = Math.max(longestLength, end - start + 1);
+    }
 
-    // Implementation of longestSubstring function
+    return longestLength;
 }
-
 module.exports = { longestSubstring };
